ea1a21f27d088e4d47ec8a294b3eb9074d999cee
Ian Denhardt <ian@zenhack.net>
2015-04-27 19:41:15 -0400

    Move example app to examples/

--- examples/hello/hello.py
+++ examples/hello/hello.py
@@ -1,7 +1,8 @@
 from gtkclassbuilder import from_filename
 from gi.repository import Gtk
+from os import path
 
-cls = from_filename('test.glade')
+cls = from_filename(path.join(path.dirname(__file__), 'hello.glade'))
 
 w1 = cls()
 w2 = cls()
