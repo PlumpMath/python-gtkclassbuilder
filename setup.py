edee77a54f0e74571d356b27342986f3d2d768f8
Ian Denhardt <ian@zenhack.net>
2015-04-27 18:47:05 -0400

    Add missing dependency on python-gobject

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import setup
 setup(name='gtkclassbuilder',
       version='0.1',
       url='https://github.com/zenhack/python-gtkclassbuilder',
-      packages=['gtkclassbuilder']
+      packages=['gtkclassbuilder'],
+      install_requires=[
+          'python-gobject>=3.16,<4.0',
+      ],
       )
